0a6fb88bdbeea78f91a7131e0663ba543a47265f
amandasuzzanne <amandasuzanne12@gmail.com>
2023-01-24 14:37:07 +0300

    Update HTML

--- resources/views/edit_task.blade.php
+++ resources/views/edit_task.blade.php
@@ -67,9 +67,12 @@ a{
     <?php endforeach; ?>
 </select>
 </div>
+</div>
 
+<div>
 <a href="/manage_tasks" class="btn btn-secondary col-sm-1">Back</a>
 <button type="submit" class="btn btn-primary col-sm-1" style="margin-left: 260px;" name="update">Update</button>
+</div>
 </form>
 
 </div>
